fe2f8d63f46972409a747034611e3c8a4b753319
nahiyan9462-beep <nahiyan9462@gmail.com>
2026-09-23 23:55:06 +0600

    Navbar

---
+++ src/app/workout/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkoutPage = () => {
+    return (
+        <div>
+            welcome to my book page.
+        </div>
+    );
+};
+
+export default WorkoutPage;
