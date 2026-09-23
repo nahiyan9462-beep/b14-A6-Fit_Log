d2433e1fa9bb557786e8aed88c6e77d72da019d8
nahiyan9462-beep <nahiyan9462@gmail.com>
2026-09-24 02:13:25 +0600

    LibraryCard

--- src/components/homepage/Library.tsx
+++ src/components/homepage/Library.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const Library = () => {
+const getLibrary = async()=>{
+    const res = await fetch('https://api.abcz.workers.dev/api/fitlog');
+    const data = await res.json();
+    return data;
+}
+
+const Library = async() => {
+    const libraryData = await getLibrary();
+    console.log(libraryData,'librarydata')
     return (
         <div>
             Library
